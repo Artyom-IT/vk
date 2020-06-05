c7de13f420a022cfc8050708dbaa31046565905d
Artyom Marchenko <prostomrartem@gmial.com>
2020-06-05 13:01:45 +0300

    added menu-close func

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ mBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   overlay.classList.toggle('disabled');
 });
+overlay.addEventListener('click', (e) => {
+  if (e.target.dataset.menu !== 'true') {
+    menu.classList.toggle('active');
+    overlay.classList.toggle('disabled');
+  }
+});
